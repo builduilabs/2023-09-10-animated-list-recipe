b6339ebbd737d280709c8669d6195485f90884ae
Sam Selikoff <sam.selikoff@gmail.com>
2023-09-11 00:32:20 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from 'next/image'
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { ArchiveBoxIcon, EnvelopeIcon } from "@heroicons/react/24/outline";
+// let i = 0;
+
+export default function Email() {
+  let [id, setId] = useState(0);
+  const [messages, setMessages] = useState([]);
+  const [selectedMessages, setSelectedMessages] = useState([]);
+  console.log({ id });
+
+  function addMessage() {
+    setMessages((messages) => [id, ...messages]);
+    setId((id) => id + 1);
+  }
+
+  function toggleMessage(mid) {
+    if (selectedMessages.includes(mid)) {
+      setSelectedMessages((messages) => messages.filter((id) => id !== mid));
+    } else {
+      setSelectedMessages((messages) => [mid, ...messages]);
+    }
+  }
+
+  function archiveSelectedMessages() {
+    setMessages((messages) =>
+      messages.filter((id) => !selectedMessages.includes(id)),
+    );
+    setSelectedMessages([]);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex h-screen flex-col items-center justify-center overscroll-y-contain px-6 py-8 text-slate-600">
+      <div className="flex h-full w-full max-w-md flex-1 flex-col rounded-2xl bg-white py-2">
+        <div className="border-b px-5">
+          <div className="flex justify-between py-2 text-right">
+            <button
+              onClick={addMessage}
+              className="-mx-2 flex items-center gap-1 rounded px-2 py-1 text-sm font-medium text-slate-400 hover:text-slate-500 active:bg-slate-100"
+            >
+              <EnvelopeIcon className="h-5 w-5 " />
+              Refresh
+            </button>
+            <button
+              onClick={archiveSelectedMessages}
+              className="-mx-2 flex items-center gap-1 rounded px-2 py-1 text-sm font-medium text-slate-400 hover:text-slate-500 active:bg-slate-100"
+            >
+              <ArchiveBoxIcon className="h-5 w-5" />
+              Archive
+            </button>
+          </div>
         </div>
+        <ul className="overflow-y-scroll px-3 pt-3">
+          <AnimatePresence initial={false}>
+            {messages.map((mid) => (
+              <motion.li
+                // initial={{ opacity: 1, height: 0, y: "-100%" }}
+                // animate={{ opacity: 1, height: "auto", y: "0%" }}
+                // initial={{ opacity: 1, height: 0, y: -48 }}
+                // animate={{ opacity: 1, height: "auto", y: 0 }}
+                initial={{ height: 0 }}
+                animate={{ height: "auto" }}
+                exit={{
+                  opacity: 0.5,
+                  height: 0,
+                  overflow: "hidden",
+                  // justifyContent: "center",
+                }}
+                // transition={{ opacity: { duration: 0.2 } }}
+                // transition={{ duration: 3 }}
+                transition={{ ease: [0.32, 0.72, 0, 1] }}
+                // transition={{ ease: [0.32, 0.72, 0, 1], duration: 5 }}
+                // transition={{ type: "tween" }}
+                key={mid}
+                data-id={mid}
+                className={`relative flex flex-col justify-end`}
+              >
+                <div className="my-4">
+                  <button
+                    onClick={() => toggleMessage(mid)}
+                    className={`${
+                      selectedMessages.includes(mid)
+                        ? "bg-blue-500"
+                        : "hover:bg-slate-200"
+                    }
+                    block w-full cursor-pointer truncate rounded px-3 py-3 text-left`}
+                  >
+                    <p
+                      className={`${
+                        selectedMessages.includes(mid)
+                          ? "text-white"
+                          : "text-slate-500"
+                      } truncate text-sm font-medium`}
+                    >
+                      Email {mid}
+                    </p>
+                  </button>
+                </div>
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </ul>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
