fd8bb700c10926f8a34c831942038b7243de2c77
Sam Selikoff <sam.selikoff@gmail.com>
2023-09-18 00:32:45 -0400

    wip

---
+++ app/v2/page.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import { useState } from "react";
+import { AnimatePresence, Transition, motion } from "framer-motion";
+import { ArchiveBoxIcon } from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/20/solid";
+
+let a = [...Array(20).keys()].reverse();
+let b = [...Array(19).keys()].reverse();
+
+const transition: Transition = {
+  // ease: "linear",
+  dduration: 50,
+  ease: [0.32, 0.72, 0, 1],
+};
+
+export default function Email() {
+  let [id, setId] = useState(a.length);
+  const [messages, setMessages] = useState(a);
+  const [selectedMessages, setSelectedMessages] = useState<number[]>([]);
+
+  function addMessage() {
+    setMessages((messages) => [id, ...messages]);
+    setId((id) => id + 1);
+  }
+
+  function toggleMessage(mid: number) {
+    if (selectedMessages.includes(mid)) {
+      setSelectedMessages((messages) => messages.filter((id) => id !== mid));
+    } else {
+      setSelectedMessages((messages) => [mid, ...messages]);
+    }
+  }
+
+  function archiveSelectedMessages() {
+    setMessages((messages) =>
+      messages.filter((id) => !selectedMessages.includes(id)),
+    );
+    setSelectedMessages([]);
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center overscroll-y-contain px-6 py-8 text-gray-200">
+      <div className="flex h-full w-full max-w-md flex-1 flex-col bg-gray-600 py-2 shadow-xl">
+        <div className="border-b border-gray-500/40 px-5">
+          <div className="flex justify-between py-2 text-right">
+            <button
+              onClick={addMessage}
+              className="-mx-2 flex items-center gap-1 rounded px-2 py-1 text-sm font-medium text-gray-400 hover:text-gray-300 active:text-gray-200"
+            >
+              <PlusIcon className="h-6 w-6" />
+            </button>
+            <button
+              onClick={archiveSelectedMessages}
+              className="-mx-2 flex items-center gap-1 rounded px-2 py-1 text-sm font-medium text-gray-400 hover:text-gray-300 active:text-gray-200"
+            >
+              <ArchiveBoxIcon className="h-5 w-5" />
+              Archive
+            </button>
+          </div>
+        </div>
+
+        <ul className="relative z-20 m-3 overflow-y-scroll">
+          <AnimatePresence initial={false}>
+            {messages.map((mid) => (
+              <motion.li
+                // initial={{ height: 0 }}
+                // animate={{ height: "auto", y: 0 }}
+                exit={{
+                  // height: 0,
+                  // zIndex: groupSelectedMessages(messages, selectedMessages)
+                  //   .reverse()
+                  //   .findIndex((group) => group.includes(mid)),
+
+                  y: -400,
+                  // y:
+                  //   -48 *
+                  //   (1 +
+                  //     getNumberOfSelectedMessagesAfter(
+                  //       messages,
+                  //       selectedMessages,
+                  //       mid,
+                  //     )),
+                }}
+                transition={transition}
+                key={mid}
+                className={`oopacity-75 fflex fflex-col jjustify-end relative z-[1000] bg-gray-600`}
+              >
+                <div>
+                  <button
+                    onClick={() => toggleMessage(mid)}
+                    className={`${
+                      selectedMessages.includes(mid)
+                        ? "bg-blue-500"
+                        : "hover:bg-gray-500/50"
+                    }
+                    block w-full cursor-pointer truncate px-8 py-4 text-left`}
+                  >
+                    <p
+                      className={`${
+                        selectedMessages.includes(mid)
+                          ? "text-white"
+                          : "text-white"
+                      } truncate text-xs text-gray-100`}
+                    >
+                      Todo {mid}
+                    </p>
+                  </button>
+                </div>
+              </motion.li>
+            ))}
+          </AnimatePresence>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function getNumberOfSelectedMessagesAfter(
+  messages: number[],
+  selectedMessages: number[],
+  id: number,
+) {
+  const startIndex = messages.indexOf(id);
+
+  if (startIndex === -1 || !selectedMessages.includes(id)) {
+    return 0;
+  }
+
+  let consecutiveCount = 0;
+
+  for (let i = startIndex + 1; i < messages.length; i++) {
+    if (selectedMessages.includes(messages[i])) {
+      consecutiveCount++;
+    } else {
+      break;
+    }
+  }
+
+  return consecutiveCount;
+}
+
+function getNumberOfSelectedMessagesBefore(
+  messages: number[],
+  selectedMessages: number[],
+  id: number,
+) {
+  const endIndex = messages.indexOf(id);
+
+  if (endIndex === -1 || !selectedMessages.includes(id)) {
+    return 0;
+  }
+
+  let consecutiveCount = 0;
+
+  for (let i = endIndex - 1; i >= 0; i--) {
+    if (selectedMessages.includes(messages[i])) {
+      consecutiveCount++;
+    } else {
+      break;
+    }
+  }
+
+  return consecutiveCount;
+}
+
+function groupSelectedMessages(messages: number[], selectedMessages: number[]) {
+  const messageGroups = [];
+  let currentGroup = [];
+
+  for (let i = 0; i < messages.length; i++) {
+    const messageId = messages[i];
+
+    if (selectedMessages.includes(messageId)) {
+      currentGroup.push(messageId);
+    } else if (currentGroup.length > 0) {
+      // If we encounter a non-selected message and there is an active group,
+      // push the current group to the result and reset it.
+      messageGroups.push(currentGroup);
+      currentGroup = [];
+    }
+  }
+
+  // Check if there's a group remaining after the loop.
+  if (currentGroup.length > 0) {
+    messageGroups.push(currentGroup);
+  }
+
+  return messageGroups;
+}
